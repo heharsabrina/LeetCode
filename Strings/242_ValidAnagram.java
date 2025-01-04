c596fe521dc2d6030ddd964bc22efcf8cc4954c0
Sabrina Hehar <hehar.sabrina01@gmail.com>
2025-01-04 21:43:27 +0530

    4/365

---
+++ Strings/242_ValidAnagram.java
@@ -0,0 +1,40 @@
+/*  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+    Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+    
+    Constraints:
+    1 <= s.length, t.length <= 5 * 104
+    s and t consist of lowercase English letters. */
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+class Solution
+{
+    public boolean isAnagram(String s, String t)
+    {
+        if (s.length()!=t.length())
+        {
+            return false;
+        }
+
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<freq.length;i++)
+        {
+            if(freq[i]!=0)
+                return false;
+        }
+        return true;
+    }
+}
