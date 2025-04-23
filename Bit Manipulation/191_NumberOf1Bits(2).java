77e545d9072d354781993a52b8bbabb40928015d
heharsabrina <hehar.sabrina01@gmail.com>
2025-04-23 07:32:28 +0530

    day 2

---
+++ Bit Manipulation/191_NumberOf1Bits(2).java
@@ -0,0 +1,15 @@
+// Time Complexity: O(k)
+// Space Complexity: O(1)
+
+class Solution
+{   public int hammingWeight(int n)
+    {
+        int count=0;
+        while(n!=0)
+        {
+            n &= (n-1);
+            count++;
+        }
+        return count;
+    }
+}
