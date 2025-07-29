cfc55852be370a8bccd76a5d682bb84ee7f80117
heharsabrina <hehar.sabrina01@gmail.com>
2025-07-29 09:58:19 +0530

    Selection sort done

--- SortingAlgorithms/SelectionSort.java
+++ SortingAlgorithms/SelectionSort.java
@@ -20,19 +20,19 @@ public class SelectionSort
     public static void main(String[] args)
     {
         int[] arr={2, 8, 5, 3, 9, 4, 1};
-        int n=arr.length, temp=0, j;
+        int n=arr.length;
 
         for(int i=0; i<n-1; i++)
         {
             int minIndex=i;
-            for(j=i+1;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
                 if(arr[j]<arr[minIndex])
                     minIndex=j;
             }
             if(minIndex!=i)
             {
-                temp=arr[i];
+                int temp=arr[i];
                 arr[i]=arr[minIndex];
                 arr[minIndex]=temp;
             }
