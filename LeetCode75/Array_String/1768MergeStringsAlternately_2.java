f4befd5c8febb7fbc5c1242da4919c5a1efc35ea
heharsabrina <hehar.sabrina01@gmail.com>
2025-08-10 13:45:28 +0530

    day 5

--- LeetCode75/Array_String/1768MergeStringsAlternately_2.java
+++ LeetCode75/Array_String/1768MergeStringsAlternately_2.java
@@ -5,20 +5,14 @@ class Solution
 {
     public String mergeAlternately(String word1, String word2)
     {
-        int i=0, j=0;
+        int m=word1.length(), n=word2.length();
         StringBuilder sb=new StringBuilder();
-        while(i<word1.length() && j<word2.length())
+        int i=0, j=0;
+
+        while(i<m || j<n)
         {
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
-        }
-        while(i<word1.length())
-        {
-            sb.append(word1.charAt(i++));
-        }
-        while(j<word2.length())
-        {
-            sb.append(word2.charAt(j++));
+            if(i<m) sb.append(word1.charAt(i++));
+            if(j<n) sb.append(word2.charAt(j++));
         }
         return sb.toString();
     }
--- LeetCode75/Array_String/1768MergeStringsAlternately_3.java
+++ LeetCode75/Array_String/1768MergeStringsAlternately_3.java
@@ -5,20 +5,13 @@ class Solution
 {
     public String mergeAlternately(String word1, String word2)
     {
-        int i=0, j=0;
+        int m=word1.length(), n=word2.length();
         StringBuilder sb=new StringBuilder();
-        while(i<word1.length() && j<word2.length())
+        
+        for(int i=0; i<Math.max(m, n); i++)
         {
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
-        }
-        while(i<word1.length())
-        {
-            sb.append(word1.charAt(i++));
-        }
-        while(j<word2.length())
-        {
-            sb.append(word2.charAt(j++));
+            if(i<m) sb.append(word1.charAt(i));
+            if(i<n) sb.append(word2.charAt(i));
         }
         return sb.toString();
     }
