559dc45f1d9ed7a5b0db9f1fa2a3efc9d86c4746
heharsabrina <hehar.sabrina01@gmail.com>
2025-08-16 23:12:35 +0530

    easy 1

---
+++ EasyCollection_TopInterviewQuestions/Strings/344ReverseString.java
@@ -0,0 +1,20 @@
+package EasyCollection_TopInterviewQuestions.Strings;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution
+{
+    public void reverseString(char[] s)
+    {
+        int i=0, j=s.length-1;
+        while(i<j)
+        {
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
